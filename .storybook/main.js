4a0112ba6fbe7f2676581ce9ef9ea3c1b2361bc7
Matthew <mahan.mattr@gmail.com>
2022-10-18 08:46:21 -0400

    Addon Config

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    'storybook-addon-designs', // 👈 Addon is registered here
   ],
   "framework": "@storybook/vue"
 }
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,9 +1,12 @@
+import { withDesign } from 'storybook-addon-designs';
 import MyButton from './Button.vue';
+
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: MyButton,
+  decorators: [withDesign],
   // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -25,22 +28,42 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/vue/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Button',}
+  Primary.parameters = {
+    design: {
+      type: 'figma',
+      url: 'iframe.html?designEmbed=true&singleStory=true&id=example-button--primary',
+    },
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'Button',}
+  Secondary.parameters = {
+    design: {
+      type: 'figma',
+      url: 'iframe.html?designEmbed=true&singleStory=true&id=example-button--primary',
+    },
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  label: 'Button',}
+  Large.parameters = {
+    design: {
+      type: 'figma',
+      url: 'iframe.html?designEmbed=true&singleStory=true&id=example-button--primary',
+    },
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Button',
+  label: 'Button',}
+  Small.parameters = {
+    design: {
+      type: 'figma',
+      url: 'iframe.html?designEmbed=true&singleStory=true&id=example-button--primary',
+    },
 };
