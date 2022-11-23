94b2cf4a848d1a7c5fd191085de8bc6bfe21829e
Matthew <mahan.mattr@gmail.com>
2022-11-23 08:31:13 -0500

    Modal Addition

--- src/stories/modal.stories.js
+++ src/stories/modal.stories.js
@@ -3,11 +3,11 @@ import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import regular-modal from '../components/modal.vue';
+import modal from '../components/mymodal.vue';
 
 storiesOf('Molecule/Modal', module)
   .add('with text', () => ({
-    components: { regular-modal },
-    template: '<regular-modal @click="action">Button</regular-modal>',
+    components: { modal },
+    template: '<modal @click="action">Button</modal>',
     methods: { action: action('clicked') }
   }));
